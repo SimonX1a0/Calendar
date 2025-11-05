20b0e213383c625f213c0b5dd202c1631954ef98
SimonX1ao <simonxiao81@gmail.com>
2025-11-05 18:38:24 -0500

    edited the display of the event bar

--- calendar-app/src/components/CalendarApp.jsx
+++ calendar-app/src/components/CalendarApp.jsx
@@ -33,7 +33,7 @@ const CalendarApp = () => {
                     <span>09</span>
                     <span>10</span>
                     <span>11</span>
-                    <span>12</span>
+                    <span className="current-day">12</span>
                     <span>13</span>
                     <span>14</span>
                     <span>15</span>
